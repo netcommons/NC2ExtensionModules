3cd13173d3524e06e4928737d6761122de793f5b
ohga <ohga.ryohei@withone.co.jp>
2015-03-18 11:58:52 +0900

    URL項目にプロトコルチェックを追加

--- banner/validator/Validator_UrlRequired.class.php
+++ banner/validator/Validator_UrlRequired.class.php
@@ -27,19 +27,65 @@ class Banner_Validator_UrlRequired extends Validator
 	{
 		if (array_key_exists('link_url', $attributes)
 			&& $attributes['banner_type'] != BANNER_TYPE_SOURCE_VALUE
-			&& (empty($attributes['link_url'])
-				|| $attributes['link_url'] == BANNER_DEFAULT_URL)) {
-			return $errStr;
+		) {
+			if (empty($attributes['link_url'])
+				|| $attributes['link_url'] == BANNER_DEFAULT_URL
+			) {
+				return $errStr;
+			}
+			if (!$this->_validateProtocol($attributes['link_url'])) {
+				$container =& DIContainerFactory::getContainer();
+				$filterChain =& $container->getComponent("FilterChain");
+				$smartyAssign =& $filterChain->getFilterByName("SmartyAssign");
+				return sprintf(_FORMAT_WRONG_ERROR, $smartyAssign->getLang('banner_link_url'));
+			}
 		}
 
 		if (array_key_exists('image_url', $attributes)
 			&& $attributes['banner_type'] == BANNER_TYPE_URL_VALUE
-			&& (empty($attributes['image_url'])
-				|| $attributes['image_url'] == BANNER_DEFAULT_URL)) {
-			return $errStr;
+		) {
+			if (empty($attributes['image_url'])
+				|| $attributes['image_url'] == BANNER_DEFAULT_URL
+			) {
+				return $errStr;
+			}
+			if (!$this->_validateProtocol($attributes['image_url'])) {
+				$container =& DIContainerFactory::getContainer();
+				$filterChain =& $container->getComponent("FilterChain");
+				$smartyAssign =& $filterChain->getFilterByName("SmartyAssign");
+				return sprintf(_FORMAT_WRONG_ERROR, $smartyAssign->getLang('banner_image_url'));
+			}
 		}
 
 		return;
+	}
+
+	/**
+	 * プロトコルチェック
+	 *
+	 * @param   string $url
+	 * @return  boolean
+	 * @access  private
+	 */
+	function _validateProtocol($url)
+	{
+		$container =& DIContainerFactory::getContainer();
+		$db =& $container->getComponent("DbObject");
+		$sql = "SELECT protocol FROM {textarea_protocol}";
+		$protocolArr = $db->execute($sql);
+		if ($protocolArr === false) {
+			return false;
+		}
+
+		if (preg_match("/^\.\//", $url) || preg_match("/^\.\.\//", $url)) {
+			return true;
+		}
+		foreach ($protocolArr as $i=>$protocol) {
+			if (preg_match("/^" . $protocol["protocol"] . "/", $url)) {
+				return true;
+			}
+		}
+		return false;
 	}
 }
 ?>
